f77868eb4196c940f83c88aad082a598ab8a5c9e
Sergey Petrov <sergeypetrovmtgrey@gmail.com>
2023-01-24 21:04:32 +0300

    Initial Commit

---
+++ BreathCounter/BreathCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathCounterApp.swift
+//  BreathCounter
+//
+//  Created by Sergey Petrov on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct BreathCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
